9e83759b50db3d05a00a6b75a33c655ae4d5d2de
Tomás R. Pita <tomasrpita@gmail.com>
2023-07-06 16:27:06 +0200

    Gets orders by contry and COD

--- app.py
+++ app.py
@@ -5,26 +5,47 @@ from datetime import datetime, timedelta
 
 load_dotenv()
 
-API_KEY = os.getenv("API_KEY_ES")
-PASSWORD = os.getenv("PASSWORD_ES")
-SHOP_NAME = os.getenv("SHOP_ES")
+shops = ["ES", "FR", "IT", "NL"]
 
-shop_url = f"https://{API_KEY}:{PASSWORD}@{SHOP_NAME}.myshopify.com/admin"
+# "Cash on Delivery" (COD) 
+cod = ["ES", "IT"]
+
 
 def main():
     one_day_ago = datetime.now() - timedelta(days=1)
 
     # Format that Shopify expects
     one_day_ago_iso = one_day_ago.isoformat()
+    
+    sku_all_counts = {}
 
-    shopify.ShopifyResource.set_site(shop_url)
-    orders = shopify.Order.find(financial_status="paid", fulfillment_status="unfulfilled", created_at_min=one_day_ago_iso)
+    for shop in shops:
 
-    sku_counts = {}
-    for order in orders:
-        for line_item in order.line_items:
-            sku_counts[line_item.sku] = sku_counts.get(line_item.sku, 0) + line_item.quantity
-    # print(sku_counts)
+        API_KEY = os.getenv(f"API_KEY_{shop}")
+        PASSWORD = os.getenv(f"PASSWORD_{shop}")
+        SHOP_NAME = os.getenv(f"SHOP_{shop}")
+        shop_url = f"https://{API_KEY}:{PASSWORD}@{SHOP_NAME}.myshopify.com/admin"
+        
+        shopify.ShopifyResource.set_site(shop_url)
+        
+        paid_orders = shopify.Order.find(financial_status="paid", fulfillment_status="unfulfilled", created_at_min=one_day_ago_iso)
+        
+        if shop in cod:
+            pending_orders = shopify.Order.find(financial_status="pending", fulfillment_status="unfulfilled", created_at_min=one_day_ago_iso)
+            cod_orders = [order for order in pending_orders if 'COD' in order.tags.split(', ')]
+            orders = paid_orders + cod_orders
+        else:
+            orders = paid_orders
+        
+        sku_counts = {}
+        for order in orders:
+            for line_item in order.line_items:
+                if line_item.sku.startswith("DIVAIN"):
+                    sku_counts[line_item.sku] = sku_counts.get(line_item.sku, 0) + line_item.quantity
+
+        sku_all_counts[shop] = sku_counts
+
+    print(sku_all_counts)
 
 if __name__ == "__main__":
     main()
